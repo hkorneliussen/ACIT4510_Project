7d9ac2e03e43a116d342905d9a147e3427c9151e
hkorneliussen <99089340+hkorneliussen@users.noreply.github.com>
2022-11-01 19:59:12 +0100

    Update gradient_boosting_regression.py

--- gradient_boosting_regression.py
+++ gradient_boosting_regression.py
@@ -26,10 +26,48 @@ import matplotlib.pyplot as plt
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 12, 4
 
-#checking of dataset exists, if not - downloading it 
+import os
+import sys
+import time
 
+#checking of dataset exists, if not exit
 path = 'dataset/'
 if not os.path.exists(path):
-  print('no')
-else:
-  print('yes')
+  print('Please run "setting up environment" cells to download dataset')
+  sys.exit()
+
+#loading original dataset
+original_dataset_path = 'dataset/audio_features.csv'
+original_dataset = pd.read_csv(original_dataset_path, index_col=[0])
+
+#loading numerical dataset
+numerical_dataset_path = 'dataset/embedded_features.csv'
+numerical_dataset = pd.read_csv(numerical_dataset_path, index_col=[0])
+
+#loading resampled dataset
+resampled_dataset_train_path = 'dataset/train.csv'
+resampled_dataset_test_path = 'dataset/test.csv'
+resampled_dataset_train = pd.read_csv(resampled_dataset_train_path, index_col=[0])
+resampled_dataset_test = pd.read_csv(resampled_dataset_test_path, index_col=[0])
+
+# applying gradient boosting regression on original dataset
+
+#dropping cateogircal (object) data
+original = original_dataset.drop(['Genre', 'Name', 'Album', 'Artist', 'Release_date'], axis=1)
+
+#applying normalization
+scaler = preprocessing.MinMaxScaler()
+names = original.columns
+d = scaler.fit_transform(original)
+original_scaled = pd.DataFrame(d, columns=names)
+original_scaled.head(3)
+
+#initializing features and target 
+X, y = original_scaled.drop('Popularity', axis=1), original_scaled['Popularity']
+#using 5 fold cross validation to split the dataset into a training set and a validation set 
+kf = KFold(n_splits=5, random_state=42, shuffle=True)
+
+for train_index, val_index in kf.split(X):
+  X_train, X_val = X.iloc[train_index], X.iloc[val_index]
+  y_train, y_val = y.iloc[train_index], y.iloc[val_index]
+
